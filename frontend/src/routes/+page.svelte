81ddd6853a5947472c67fc45c9dc4df16050314f
kanngji <alleneffort1994@gmail.com>
2024-04-24 02:57:34 +0900

    (front)nav , todo

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<style>
+    /* 바의 스타일 */
+    nav {
+      background-color: #7ec8e3; /* 하늘색 */
+      padding: 10px 20px; /* 위아래 10px, 좌우 20px의 패딩 */
+    }
+  
+    ul {
+      margin: 0;
+      padding: 0;
+      list-style-type: none;
+    }
+  
+    li {
+      display: inline;
+      margin-right: 10px;
+    }
+  
+    a {
+      color: white;
+      text-decoration: none;
+    }
+  
+    a:hover {
+      text-decoration: underline;
+    }
+  </style>
+  
+  <main>
+    <!-- todo: logo제작, 위치조정, 사용방법 -->
+    
+    <!-- 네비게이션 바 -->
+    <nav>
+      <ul>
+        <li><a href="/">Main</a></li>
+        <li><a href="/list">List</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  
+    
+  </main>
